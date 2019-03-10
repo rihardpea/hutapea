837575d569cd4b15dfe57fad04b6477346691b8c
rihardpea <48405061+rihardpea@users.noreply.github.com>
2019-03-10 17:19:23 +0700

    Add files via upload

---
+++ cracking.sh
@@ -0,0 +1,130 @@
+#!/system/xbin/bash
+#Author 
+#Mau Ngapin Lu nyet!!
+#Ga Usah Recode Anjing!!
+
+clear
+clear
+echo
+echo "=================================================="
+figlet -f slant CRACKING | lolcat
+date | lolcat
+echo "=================================================="
+echo
+echo "#################################"
+echo " [+] TOOLS FB CRACK " | lolcat
+echo " [+] CREATE BY HUTAPEA " | lolcat
+echo " [+] FB : YIZREEL " | lolcat
+echo "#################################"
+echo
+echo "[#############################]" | lolcat
+echo $"[1] CRACK FACEBOOK TARGET"
+echo "[#############################]" | lolcat
+echo $"[2] CRACK FACEBOOK MBF NEW"
+echo "[#############################]" | lolcat
+echo $"[3] CREATE WORDLIST NEW"
+echo "[#############################]" | lolcat
+echo $"[0] KELUAR "
+echo "[#############################]" | lolcat
+read -p "[+]====[Pilih Nomer]
+[+]=====>" gg
+
+
+if [ $gg = 1 ] || [ $gg = 1 ]
+
+
+then
+
+
+clear
+
+pkg install git
+
+pkg install python2
+
+pkg install --upgrade pip
+
+pkg install mechanize
+
+git clone https://github.com/rihardpea/FBNEW
+
+cd FBNEW
+
+python2 hutapea.py
+
+
+fi
+
+
+if [ $gg = 2 ] || [ $gg = 2 ]
+
+
+then
+
+
+clear
+
+pkg install python2
+
+pip2 install --upgrade pip
+
+pip2 install mechanize
+
+git clone https://github.com/rihardpea/mbf
+
+cd mbf
+
+sleep1
+
+python2 mbf.py
+
+
+fi
+
+
+if [ $gg = 3 ] || [ $gg = 3 ]
+
+
+then
+
+
+clear
+
+termux-setup-storage
+
+cd /storage/emulated/0/
+
+pkg install git
+
+pkg install python2
+
+git clone https://github.com/Mebus/cupp.git
+
+cd cupp
+
+sleep1
+
+python2 cupp.py
+
+
+fi
+
+
+if [ $gg = 0 ] || [ $gg = 0 ]
+
+
+then
+
+
+echo "KELUAR"|lolcat
+
+echo "THANKS SUDAH MENGGUNAKAN TOOLS INI.."|lolcat
+
+figlet "THANKS MRA27"|lolcat
+
+sleep 1
+
+exit
+
+fi
+
